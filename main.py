5463f395498af7552c9eec61b37647b73afa8743
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2022-01-07 13:52:46 +0000

    Done.

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ async def api_product(name: str, price: float, background_tasks: BackgroundTasks
     execution_time = f'{round(time_diff.total_seconds() * 1000)} ms'
     return {'message': 'Success', 'data': data, 'execution_time': execution_time}
 
+
 @app.post("/api/v1/data")
 async def api_data(name: str, price: float, background_tasks: BackgroundTasks, description: Optional[str] = None):
     start_time = datetime.datetime.now()
@@ -33,9 +34,3 @@ async def api_data(name: str, price: float, background_tasks: BackgroundTasks, d
     time_diff = (end_time - start_time)
     execution_time = f'{round(time_diff.total_seconds() * 1000)} ms'
     return {'message': 'Success', 'data': data, 'execution_time': execution_time}
-
-@app.get("/data")
-def get_data():
-    with open("data.json", "r+") as file:
-        data = json.load(file)
-        return data
